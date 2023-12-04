7de0a5126ff9b4c30dc47edc2166b62354147393
Mohd Faraz <farazmohd0701@gmail.com>
2023-12-04 23:06:45 +0530

    RandomImage

---
+++ src/App.js
@@ -0,0 +1,21 @@
+// import logo from './logo.svg';
+import './App.css';
+import Display from './Component/Display';
+import Home from './Component/Home';
+import { BrowserRouter as Router,Link,Route,Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+<Router>
+  <Routes>
+    <Route path="/" element={<Home/>}></Route>
+    <Route path="/display" element={<Display/>}></Route>
+  </Routes>
+</Router>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/Display.js
@@ -0,0 +1,11 @@
+import React from "react";
+function Display()
+{
+    return(
+        <div>
+            <h3>Shared Image</h3>
+            <img src="https://pbs.twimg.com/profile_images/1651506701957230593/mqp5OM-s_400x400.jpg" style={{width:'250px',height:'250pxgg'}}></img>
+        </div>
+    );
+}
+export default Display;
